199e6e36882bd046dfd35fd41c056a8a9b120442
Lana Avery <memekoten@gmail.com>
2024-04-29 09:40:33 +1000

    Release

---
+++ 1.5/Source/SearchAndDestroy/JobGiver_AIFightEnemiesShortExp.cs
@@ -0,0 +1,40 @@
+﻿using RimWorld;
+using SearchAndDestroy.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using Verse.AI;
+
+namespace SearchAndDestroy
+{
+    class JobGiver_AIFightEnemiesShortExp : JobGiver_AIFightEnemies
+    {
+		protected override Job TryGiveJob(Pawn pawn)
+        {
+            var originalResponse = pawn.playerSettings.hostilityResponse;
+            pawn.playerSettings.hostilityResponse = HostilityResponseMode.Attack;
+			var job = base.TryGiveJob(pawn);
+            if (job != null)
+            {
+				job.expiryInterval = 30;
+            }
+            pawn.playerSettings.hostilityResponse = originalResponse;
+            return job;
+		}
+        protected override bool ExtraTargetValidator(Pawn pawn, Thing target)
+        {
+            var targetPawn = target as Pawn;
+            if(targetPawn == null)
+            {
+                return false;
+            }
+            if(targetPawn.NonHumanlikeOrWildMan() && !targetPawn.IsAttacking())
+            {
+                return false;
+            }
+            return base.ExtraTargetValidator(pawn, target);
+        }
+    }
+}
