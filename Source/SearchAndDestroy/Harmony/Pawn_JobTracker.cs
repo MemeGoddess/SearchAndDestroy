f7e27b93f20b8c8ad2218b48022a096aacc9388c
Lana <memekoten@gmail.com>
2024-10-18 19:22:20 +1100

    Moved to global source folder with zipped backups (#2)

--- Source/SearchAndDestroy/Harmony/Pawn_JobTracker.cs
+++ Source/SearchAndDestroy/Harmony/Pawn_JobTracker.cs
@@ -1,9 +1,6 @@
-﻿using Harmony;
+﻿using HarmonyLib;
 using SearchAndDestroy.Storage;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Verse;
 using Verse.AI;
 
@@ -22,7 +19,7 @@ namespace SearchAndDestroy.Harmony
                 if(pawnData.SD_enabled && __instance.jobQueue.Count > 0)
                 {
                     QueuedJob qjob = __instance.jobQueue.Last();  
-                    __instance.ClearQueuedJobs();
+                    __instance.ClearQueuedJobs(false);
                     __result = new ThinkResult(qjob.job, __result.SourceNode, null, false);
                 }
             }
