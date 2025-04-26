3031a3c08851b47b2e75058a3a8538f14452a834
Lana Avery <memekoten@gmail.com>
2025-04-26 16:23:51 +1000

    reference cleanup

--- Source/SearchAndDestroy/JobGiver_AIFightEnemiesShortExp.cs
+++ Source/SearchAndDestroy/JobGiver_AIFightEnemiesShortExp.cs
@@ -1,10 +1,4 @@
 ﻿using RimWorld;
-using SearchAndDestroy.Storage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
 using Verse;
 using Verse.AI;
 
